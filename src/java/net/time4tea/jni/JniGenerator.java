fb5644ed73b19a5c362a6c1c18dd3d7fba9b160f
time4tea <james@time4tea.net>
2013-05-17 16:04:40 +0100

    find class files in hierarchy

--- src/java/net/time4tea/jni/JniGenerator.java
+++ src/java/net/time4tea/jni/JniGenerator.java
@@ -1,5 +1,9 @@
 package net.time4tea.jni;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
@@ -7,7 +11,9 @@ import org.objectweb.asm.Opcodes;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -18,27 +24,81 @@ public class JniGenerator {
 
     public static void main(String[] args) throws IOException, Templates.TemplateProcessingException {
 
-        NativeClassParser parser = new NativeClassParser();
+        File input = new File("xxx");
+
+        String outputFilename = "foobar.c";
+
+        FileWriter fileWriter = new FileWriter(new File(outputFilename));
+
+        List<File> filepaths = collectClassesFrom(input);
+
+        final NativeClassParser parser = new NativeClassParser();
 
         Templates templates = new Templates(JniGenerator.class);
 
         Map<String, Object> model = newHashMap();
 
+
+
+        model.put("filename", outputFilename);
+        model.put("date", new Date());
+
         model.put("classes", newArrayList(
-                parser.parse(new File("PlannerBookedProgrammeImpl.class")),
-                parser.parse(new File("PlannerSessionImpl.class"))
+                Iterables.filter(
+                Lists.transform(filepaths, new Function<File, NativeClass>() {
+                    @Override
+                    public NativeClass apply(File filename) {
+                        try {
+                            return parser.parse(filename);
+                        } catch (IOException e) {
+                            throw new RuntimeException("dammit",e);
+                        }
+                    }
+                }), new Predicate<NativeClass>() {
+                    @Override
+                    public boolean apply(NativeClass o) {
+                        return o.hasNativeMethods();
+                    }
+                })
         ));
 
-        String process = templates.process("decl.ftl", model);
-        System.out.println(process);
+        templates.process("decl.ftl", model, fileWriter);
+    }
+
+    private static List<File> collectClassesFrom(File input) {
+
+        if ( input.isFile()) {
+            return newArrayList(input);
+        }
+
+        List<File> files = newArrayList();
+        addClassFilesIn(files, input);
+        return files;
+    }
+
+    private static void addClassFilesIn(List<File> files, File input) {
+        File[] directory = input.listFiles();
+        if ( directory == null ) {
+            return;
+        }
+
+        for (File file : directory) {
+            if ( file.isDirectory() ) {
+                addClassFilesIn(files, file);
+            }
+            else if ( file.getName().endsWith(".class")) {
+                files.add(file);
+            }
+        }
     }
 
     public static class NativeClassParser {
         public NativeClass parse(File file) throws IOException {
             ClassReader classReader = new ClassReader(new FileInputStream(file));
             F f = new F();
-            classReader.accept(f, ClassReader.SKIP_DEBUG);
+            classReader.accept(f, ClassReader.EXPAND_FRAMES);
             return new NativeClass(
+                    f.className,
                     file.getName(),
                     file.getName().toLowerCase().replaceAll("\\.", "_"),
                     f.methods
@@ -48,14 +108,20 @@ public class JniGenerator {
 
     public static class NativeClass {
 
+        public final String className;
         public final String internalName;
         public final String decl;
         public final List<NativeMethod> methods;
 
-        public NativeClass(String internalName, String decl, List<NativeMethod> methods) {
+        public NativeClass(String className, String internalName, String decl, List<NativeMethod> methods) {
+            this.className = className;
             this.internalName = internalName;
             this.decl = decl;
             this.methods = methods;
+        }
+
+        public String getClassName() {
+            return className;
         }
 
         public String getInternalName() {
@@ -68,6 +134,10 @@ public class JniGenerator {
 
         public List<NativeMethod> getMethods() {
             return methods;
+        }
+
+        public boolean hasNativeMethods() {
+            return ! methods.isEmpty();
         }
     }
 
@@ -94,10 +164,16 @@ public class JniGenerator {
 
     public static class F extends ClassVisitor {
 
+        private String className;
         public final List<NativeMethod> methods = newArrayList();
 
         public F() {
             super(Opcodes.ASM4);
+        }
+
+        @Override
+        public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+            this.className = name.replaceAll("/", ".");
         }
 
         @Override
@@ -109,7 +185,7 @@ public class JniGenerator {
                 ));
             }
 
-            return super.visitMethod(access, name, desc, signature, exceptions);
+            return null;
         }
     }
 }
